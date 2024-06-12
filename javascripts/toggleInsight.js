cd6d5cf3082c309c2ad26ffb17a58db1c29245c6
Nikhil Reddy <nikhilreddy.maligireddy1@gmail.com>
2024-06-12 18:07:25 -0400

    done

--- javascripts/toggleInsight.js
+++
@@ -1,8 +0,0 @@
-function toggleInsight(button) {
-    var insight = button.nextElementSibling;
-    if (insight.style.display === "none") {
-        insight.style.display = "block";
-    } else {
-        insight.style.display = "none";
-    }
-}
